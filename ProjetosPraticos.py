cb3ed80e5d1c98a933d7111e6bf833cbed4f137f
Joao Nunes <jv.nunes@tradein.com.br>
2025-02-04 14:55:49 -0300

    .

--- ProjetosPraticos.py
+++ ProjetosPraticos.py
@@ -1,6 +1,5 @@
 from functions_mlp import *
 
-
 print("Projetro prático 01")
 arquivos_treinamento = [
     {'nome': 'Seção5.8_RNA' , 'caminho':"Dados\\Treinamento\\Tabela#Seção5.8_RNA.txt" },
@@ -8,10 +7,16 @@ arquivos_treinamento = [
     {'nome': 'Seção5.10_RNA', 'caminho':"Dados\\Treinamento\\Tabela#Seção5.10_RNA.txt"}
 ]
 
+arquivos_treinamento = [
+    {'nome': 'Seção5.8_RNA' , 'caminho':"Dados/Treinamento/Tabela#Seção5.8_RNA.txt" },
+    {'nome': 'Seção5.9_RNA' , 'caminho':"Dados/Treinamento/Tabela#Seção5.9_RNA.txt" },
+    {'nome': 'Seção5.10_RNA', 'caminho':"Dados/Treinamento/Tabela#Seção5.8_RNA.txt"}
+]
+
 colunas     = ["Arquivo", "Pesos Finais", "Biases Finais"]
-p      = []
-b      = []
 resultados  = []
+p           = []
+b           = []
 
 for arquivo in arquivos_treinamento:
     X, Y = load_data(arquivo['caminho'])
@@ -44,25 +49,31 @@ arquivos_teste = [
     {'nome': '5.7_RNA' , 'caminho':"Dados\\Teste\\Tabela#5.7_RNA.txt" }
 ]
 
+arquivos_teste = [
+    {'nome': '5.3_RNA' , 'caminho':"Dados/Teste/Tabela#5.3_RNA.txt" },
+    {'nome': '5.5_RNA' , 'caminho':"Dados/Teste/Tabela#5.5_RNA.txt" },
+    {'nome': '5.7_RNA' , 'caminho':"Dados/Teste/Tabela#5.7_RNA.txt" }
+]
+
 resultados_teste = []
 
 
-for arquivo, pesos_finais, biases_finais in zip(arquivos_teste, p, b):
-    X_teste, Y_teste = load_data(arquivo['caminho'])
-    # print(f' {arquivo["nome"]}\n X_teste = {X_teste}\n Y_teste = {Y_teste} ')
+# for arquivo, pesos_finais, biases_finais in zip(arquivos_teste, p, b):
+#     X_teste, Y_teste = load_data(arquivo['caminho'])
+#     # print(f' {arquivo["nome"]}\n X_teste = {X_teste}\n Y_teste = {Y_teste} ')
 
-    if isinstance(pesos_finais, list):
-        pesos_finais = [np.array(p) if not isinstance(p, np.ndarray) else p for p in pesos_finais]
+#     if isinstance(pesos_finais, list):
+#         pesos_finais = [np.array(p) if not isinstance(p, np.ndarray) else p for p in pesos_finais]
 
-    if isinstance(biases_finais, list):
-        biases_finais = [np.array(b) if not isinstance(b, np.ndarray) else b for b in biases_finais]
+#     if isinstance(biases_finais, list):
+#         biases_finais = [np.array(b) if not isinstance(b, np.ndarray) else b for b in biases_finais]
 
-    previsao = testar(X_teste, pesos_finais, biases_finais)
+#     previsao = testar(X_teste, pesos_finais, biases_finais)
 
-    erro = np.mean((previsao - Y_teste) ** 2)
+#     erro = np.mean((previsao - Y_teste) ** 2)
 
-    resultados_teste.append([arquivo['nome'], erro, previsao])
+#     resultados_teste.append([arquivo['nome'], erro, previsao])
 
-colunas_teste = ["Arquivo", "Erro MSE", "Previsões"]
-print("Resultados do Teste:")
-print(tabulate(resultados_teste, headers=colunas_teste, tablefmt="grid"))
+# colunas_teste = ["Arquivo", "Erro MSE", "Previsões"]
+# print("Resultados do Teste:")
+# print(tabulate(resultados_teste, headers=colunas_teste, tablefmt="grid"))
--- functions_mlp.py
+++ functions_mlp.py
@@ -1,6 +1,6 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+# import pandas as pd
+# import matplotlib.pyplot as plt
 
 from tabulate import tabulate  
 
@@ -41,20 +41,19 @@ def treinamento(X, Y, layers, epocas=10000, taxa_aprendizagem=0.1, precisao=1e-6
     biases  = [np.random.rand(layers[i], 1)             - 0.5 for i in range(1, len(layers))]
 
     for epoch in range(epocas):
-        # print(f'Epoch {epoch} weights = {len(weights)} biases = {len(biases)}')
-        activations = [X.T]
+        ativacoes = [X.T]
         for w, b in zip(weights, biases):
-            z = np.dot(w, activations[-1]) + b
-            activations.append(sigmoid(z))
+            z = np.dot(w, ativacoes[-1]) + b
+            ativacoes.append(sigmoid(z))
 
-        deltas = [activations[-1] - Y.T]
+        deltas = [ativacoes[-1] - Y.T]
         for i in range(len(layers) - 2, 0, -1):
-            deltas.append(np.dot(weights[i].T, deltas[-1]) * derivada_sigmoid(activations[i]))
+            deltas.append(np.dot(weights[i].T, deltas[-1]) * derivada_sigmoid(ativacoes[i]))
 
         deltas.reverse()
 
         for i in range(len(weights)):
-            weights[i] -= taxa_aprendizagem * np.dot(deltas[i], activations[i].T)
+            weights[i] -= taxa_aprendizagem * np.dot(deltas[i], ativacoes[i].T)
             biases[i]  -= taxa_aprendizagem * np.sum(deltas[i], axis=1, keepdims=True)
 
         if np.max(np.abs(deltas[-1])) < precisao:
@@ -67,8 +66,6 @@ def testar(X, pesos, biases):
     ativacoes = [X]
     for w, b in zip(pesos, biases):
         b = b.T  
-        # print(f"Peso: {w.shape} Bias: {b.shape}")
-        # print("Ativação anterior:", ativacoes[-1].shape)
         z = np.dot(ativacoes[-1], w.T) + b
         a = 1 / (1 + np.exp(-z)) 
         ativacoes.append(a)
